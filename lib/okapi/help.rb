372425c3602d686317da446b1a2da72724997abf
Tully <tully@apiary.io>
2013-05-07 18:52:44 +0200

    help fixed

--- lib/okapi/help.rb
+++ lib/okapi/help.rb
@@ -16,7 +16,7 @@ module Apiary
         puts "\nCurrently available okapi options are:\n\n"
 
         puts "\t-b, --blueprint             path to the blueprint (default: " + BLUEPRINT_PATH + " )"
-        puts "\t-t, --test_spec             path to the test specification (default: " + TEST_SPEC_PATH + " )"
+        puts "\t-t, --test_spec             path to the test specification (default: " + TEST_SPEC_PATHS + " )"
         #puts "\t-o, --output                output format (default: " + OUTPUT + ")"
         puts "\t-u, --test_url              url to test (default: " + TEST_URL + ")"
         puts "\t-a, --apiary                apiary url  (default: " + APIARY_URL + ")"
